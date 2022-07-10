5183414012b8de375655c1240dfd33f25d6ee436
srchang8 <srchang8@gmail.com>
2022-07-10 15:55:23 -0400

    Time: 17 ms (33.41%), Space: 56.4 MB (18.94%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -0,0 +1,105 @@
+class Solution {
+    
+    /*
+        
+        not too difficult but understand why BFS is likely better
+        
+        Overall solution:
+        
+        check top left
+        check right bottom
+        
+        check commonality 
+        
+        
+        algorithm:
+        
+        1. add all top & left into a pacific ocean queue
+            -then do BFS and mark is reachable
+        2. add all right & bottomw into atlantic ocean
+            -then do bfs and mark as reachable
+        3. check commonaility if both reachable add to result
+        
+        */
+    
+    
+    private static final int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    private int numRows;
+    private int numCols;
+    
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        // Check if input is empty
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return new ArrayList<>();
+        }
+
+        // Save initial values to parameters
+        numRows = matrix.length;
+        numCols = matrix[0].length;
+        
+        // Setup each queue with cells adjacent to their respective ocean
+        Queue<int[]> pacificQueue = new LinkedList<>();
+        Queue<int[]> atlanticQueue = new LinkedList<>();
+        
+        boolean[][] pacificReachable = new boolean[numRows][numCols];
+        boolean[][] atlanticReachable = new boolean[numRows][numCols];
+        
+        //add verticals: left col and right col
+        for (int i = 0; i < numRows; i++) {
+            pacificQueue.offer(new int[]{i, 0});
+            atlanticQueue.offer(new int[]{i, numCols - 1});
+        }
+        
+        //add horizontal: top and bottom row
+        for (int i = 0; i < numCols; i++) {
+            pacificQueue.offer(new int[]{0, i});
+            atlanticQueue.offer(new int[]{numRows - 1, i});
+        }
+        
+        
+        // Perform a BFS for each ocean to find all cells accessible by each ocean
+        bfs(matrix, pacificQueue, pacificReachable);
+        bfs(matrix, atlanticQueue, atlanticReachable);
+        
+        
+        // Find all cells that can reach both oceans
+        List<List<Integer>> commonCells = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (pacificReachable[i][j] && atlanticReachable[i][j]) {
+                    commonCells.add(List.of(i, j));
+                }
+            }
+        }
+        return commonCells;
+    }
+    
+    
+    
+    
+    private void bfs(int[][] grid, Queue<int[]> queue, boolean[][] visited) {
+        
+        while (!queue.isEmpty()) {
+            
+            int[] p = queue.poll();
+            
+            // This cell is reachable, so mark it
+            visited[p[0]][p[1]] = true;
+            
+            for (int[] dir : DIRECTIONS) { // Check all 4 directions
+                int yN = p[0] + dir[0];
+                int xN = p[1] + dir[1];
+                
+                // check valid neighbors
+                if (yN < 0 || yN >= numRows || xN < 0 || xN >= numCols ||
+                    visited[yN][xN] || grid[yN][xN] < grid[p[0]][p[1]]) {
+                    continue;
+                }
+            
+               
+                queue.offer(new int[]{yN, xN});
+            }
+        }
+      
+    }
+}
