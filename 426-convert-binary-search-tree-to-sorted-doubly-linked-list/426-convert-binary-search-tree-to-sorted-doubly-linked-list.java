6e0f88139b6c7ad7715d69fe1af65f6aa913c3b1
srchang8 <srchang8@gmail.com>
2022-07-13 15:50:04 -0400

    Time: 1 ms (29.27%), Space: 42.8 MB (23.52%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,3 +1,49 @@
+
+/*
+    space: o(h)  h=height of tree
+    time: o(n)
+
+*/
+class Solution {
+    public Node treeToDoublyList(Node root) {
+        
+        if (root == null) return root;
+        
+        Node dummy = new Node(-1);
+        
+        
+        Stack<Node> stack = new Stack();
+        
+        Node prev = dummy;
+        
+        while (!stack.isEmpty() || root != null){
+            
+            //left
+            while (root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            
+            //root
+            root = stack.pop();
+    
+            prev.right = root; // prev -> root
+            root.left = prev;  // prev  <- root
+            prev = root;       
+                
+            //right
+            root = root.right;
+        }
+        
+        //dummy.right = 1
+        //sow the head with tail, creating cycle
+        dummy.right.left = prev;//    5 <- 1
+        prev.right = dummy.right;// 5 -> 1
+        
+        return dummy.right;
+        
+    }
+}
 /*
 // Definition for a Node.
 class Node {
@@ -19,37 +65,29 @@ class Node {
 };
 */
 
-class Solution {
-    public Node treeToDoublyList(Node root) {
-        
-        if (root == null) return root;
-        
-        Stack<Node> stack = new Stack();
-        
-        Node dummy = new Node(-1);
-        dummy.right = root;
-        Node prevNode = dummy;
-        
-        while (!stack.isEmpty() || root != null){
-            while (root != null){
-                stack.push(root);
-                root = root.left;
-            }
-            
-            root = stack.pop();
-            
-            prevNode.right = root;
-            root.left = prevNode;
-            
-            prevNode = root;
-            
-            
-            root = root.right;
-        }
-        
-        prevNode.right = dummy.right;
-        dummy.right.left = prevNode;
-        
-        return dummy.right;
-    }
-}
+
+/*
+
+IMPORTANT: 
+    - use prev node pointer
+    - dont forget to sow the end
+
+
+simple inorder traversal just need a prevNode
+    
+    -- 3 steps for prev Node --
+    prevNode <- currNode
+    prevNode -> currNode
+    prevNode = currNode
+    
+    --Lastly create a cycle with last Node
+
+*/
+//Inorder on a BST will always give you sorted
+//prev will end as the last node
+//dummy is always used to point to head
+
+
+
+
+
