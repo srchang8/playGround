a06e608a75fece8601939b284650d2e6e5b77358
srchang8 <srchang8@gmail.com>
2022-07-25 15:39:31 -0400

    Time: 5 ms (77.28%), Space: 43.2 MB (83.36%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        
+        int left=0;
+        int right=s.length()-1;
+        boolean noChar = false;
+        
+        for (int i=0; i<s.length(); i++){
+            if (Character.isLetterOrDigit(s.charAt(i))) noChar = false;
+        }
+        if (noChar) return false;
+        
+        while (left < right){
+            
+            while (!Character.isLetterOrDigit(s.charAt(left)) && left < right) left++;
+            while (!Character.isLetterOrDigit(s.charAt(right)) && left < right) right--;
+            
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
