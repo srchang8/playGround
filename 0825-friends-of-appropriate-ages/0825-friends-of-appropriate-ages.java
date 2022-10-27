8fa18fb5432d75323c8608def3ed92b051ea00c1
srchang8 <srchang8@gmail.com>
2022-10-26 20:22:16 -0400

    Time: 1 ms (100.00%), Space: 45.6 MB (95.53%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -1,16 +1,60 @@
 class Solution {
-    public int numFriendRequests(int[] ages) {
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int age : ages)
-            count.put(age, count.getOrDefault(age, 0) + 1);
-        int res = 0;
-        for (Integer a : count.keySet())
-            for (Integer b : count.keySet())
-                if (request(a, b)) res += count.get(a) * (count.get(b) - (a == b ? 1 : 0));
-        return res;
-    }
+    
+    /*
+        Basic idea: get total possible friend requests
+                    then subtract by conditions that remove possible friend request
+                    
+        1.
+        
+        
+        
+   -- - -- - - Explanation - - - -- - 
+       You can reverse the conditions from:
+    y <= 0.5x + 7
+    y > x (third one included in this)
 
-    private boolean request(int a, int b) {
-        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
+    to:
+    y > 0.5x + 7 and y <x
+
+    This means
+    x>y>0.5x+7
+
+    Min x that holds this equation:
+    x>0.5x+7 and
+    0.5x > 7
+    x > 14
+
+    min x is 15
+
+    sumInAge[i]
+    This will give you how many people in this bracket and below.
+
+    int count = sumInAge[i] - sumInAge[i / 2 + 7];
+    This will give you how many requests can you send from this age. There could be multiple people in this age group so we need to multiply
+    count * numInAge[i]
+
+    You need to account for not sending to itself so you substract
+    count * numInAge[i] - numInAge[i]
+    gives total
+    */
+    
+    public int numFriendRequests(int[] ages) {
+        int res = 0;
+        int[] numInAge = new int[121], sumInAge = new int[121];
+        
+        for(int i : ages) 
+            numInAge[i] ++;
+        
+        for(int i = 1; i <= 120; ++i) 
+            sumInAge[i] = numInAge[i] + sumInAge[i - 1];
+        
+        for(int i = 15; i <= 120; ++i) {
+            if(numInAge[i] == 0) continue;
+            
+            // remove ages <= 0.5 * age[x] + 7
+            int count = sumInAge[i] - sumInAge[i / 2 + 7];
+            res += count * numInAge[i] - numInAge[i]; //people will not friend request themselves, so  - numInAge[i]
+        }
+        return res;
     }
 }
