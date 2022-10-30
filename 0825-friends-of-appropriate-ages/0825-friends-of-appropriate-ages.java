7e2f41a7501dabb37349db77dbfb3200bef70242
srchang8 <srchang8@gmail.com>
2022-10-30 12:17:53 -0400

    Time: 6 ms (72.46%), Space: 56.9 MB (58.82%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -4,7 +4,11 @@ class Solution {
         Basic idea: get total possible friend requests
                     then subtract by conditions that remove possible friend request
                     
-        1.
+        1. Count age occurance
+        2. get preSum
+        3. preSum[i] * numAge[i] = Answer(ages that can friend each other) but before you do that:
+            remove ages: 0.5 * age[x] + 7
+            remove self: - numAge[i]
         
         
         
@@ -26,10 +30,10 @@ class Solution {
 
     min x is 15
 
-    sumInAge[i]
+    preSumInAge[i]
     This will give you how many people in this bracket and below.
 
-    int count = sumInAge[i] - sumInAge[i / 2 + 7];
+    int count = preSumInAge[i] - preSumInAge[i / 2 + 7];
     This will give you how many requests can you send from this age. 
     
     There could be multiple people in this age group so we need to multiply
@@ -43,19 +47,19 @@ class Solution {
     public int numFriendRequests(int[] ages) {
         int res = 0;
         int[] numInAge = new int[121];
-        int[] sumInAge = new int[121];
+        int[] preSumInAge = new int[121];
         
         for(int i : ages) 
             numInAge[i] ++;
         
         for(int i = 1; i<numInAge.length; i++) 
-            sumInAge[i] = numInAge[i] + sumInAge[i - 1];
+            preSumInAge[i] = numInAge[i] + preSumInAge[i - 1];
         
         for(int i = 15; i<numInAge.length; i++) {
             if(numInAge[i] == 0) continue;
             
             // remove ages <= 0.5 * age[x] + 7
-            int count = sumInAge[i] - sumInAge[i / 2 + 7];
+            int count = preSumInAge[i] - preSumInAge[i / 2 + 7];
             
             //people will not friend request themselves, so  - numInAge[i]
             res += (count * numInAge[i]) - numInAge[i]; 
